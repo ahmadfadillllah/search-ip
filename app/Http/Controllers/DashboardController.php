94c8fc17a68f14a9dd4777e49f4e93820fa901c7
ahmadfadillllah <ahmadfadillah502@gmail.com>
2024-11-07 14:06:47 +0800

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        return view('dashboard.index');
+        $unit = DB::connection('sqlsrv')
+                    ->table('FLT_VEHICLE')
+                    ->select([
+                        'VHC_ID',
+                        'EQU_TYPEID',
+                        'NET_IPADDRESS',
+                        'APP_VERSION',
+                    ])
+                    ->where('VHC_ACTIVE', true)
+                    ->get();
+
+        return view('dashboard.index', compact('unit'));
     }
 }
